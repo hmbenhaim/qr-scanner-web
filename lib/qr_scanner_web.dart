72ed088cf5a83e56821fea739136bf9c038b53a1
hmbenhaim <hmbenhaim@gmail.com>
2021-11-21 12:52:54 +0200

    init

--- lib/qr_scanner_web.dart
+++ lib/qr_scanner_web.dart
@@ -8,13 +8,14 @@ import 'package:js/js.dart';
 import 'dart:js_util';
 import 'dart:html' as html;
 
-@JS('QrScanner.scanImage')
-external Future<String> scan(image);
+@JS('qrInImage')
+external Future<String> _scan(image);
 
 class QrScannerWeb {
   Future<String?> fromImageBytes(Uint8List imageBytes, String mimeType) async {
     final base64 = base64Encode(imageBytes);
-    final result = await promiseToFuture(scan('data:$mimeType;base64,$base64'));
+    final result =
+        await promiseToFuture(_scan('data:$mimeType;base64,$base64'));
     return result.toString();
   }
 }
